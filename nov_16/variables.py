9d304b43318fca63ea9c16792496c76dcc9c0f8c
fran <frangarcia2707@gmail.com>
2025-12-16 08:53:26 +0100

    Subir variables

---
+++ nov_16/variables.py
@@ -0,0 +1,17 @@
+nombre = "Pepe"
+edad = 68
+sueldo = 2300.50
+jubilado = False
+print("Nombre:", nombre, "Edad:", edad, "Jubilado:", jubilado, "Sueldo:", sueldo)
+a,b=10,5
+suma = a + b
+print("La suma de", a, "y", b, "es", suma)
+a=b=c=10
+print("Valor de a:", a, "Valor de b:", b, "Valor de c:", c)
+print(a,b,c, sep=", ")
+print('''Línea 1
+Línea 2
+Línea 3
+Linea 4''')
+print("Linea 1\nLínea 2\nLínea 3\nLínea 4")
+print(f"Nombre: {nombre}, Edad: {edad}, Jubilado: {jubilado}, Sueldo: {sueldo}")
