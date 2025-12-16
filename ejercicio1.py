a6487a2eafbf4d005751d4b71a1278ee505548fd
fran <frangarcia2707@gmail.com>
2025-12-16 09:28:29 +0100

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+matematicas=3.0
+lengua=7.5
+quimica=10.0
+print(f"{'Matemáticas':>15}: {matematicas:>4}\n{'Lengua':>15}: {lengua:>4}\n{'Química':>15}: {quimica:>4}\n{'*'*22}\n{'Media':>15}: {(matematicas+lengua+quimica)/3:>5.2f}")
--- nov_16/variables.py
+++ nov_16/variables.py
@@ -15,3 +15,13 @@ Línea 3
 Linea 4''')
 print("Linea 1\nLínea 2\nLínea 3\nLínea 4")
 print(f"Nombre: {nombre}, Edad: {edad}, Jubilado: {jubilado}, Sueldo: {sueldo}")
+nombre = "Fran"
+Apellido = "García"
+print(f"Nombre completo: {nombre} {Apellido}") # Interpolación de variables, f-string
+print("Nombre completo: {} {}".format(nombre, Apellido)) # Método format, Fran García
+print("Nombre completo: {1} {0}".format(nombre, Apellido)) # Método format con índices, García Fran
+print("Nombre completo: {n} {a}".format(n=nombre, a=Apellido)) # Método format con nombres, Fran García
+print("Sueldo con dos decimales: {:.2f}".format(sueldo)) # Sueldo con dos decimales: 2300.50
+nombre = "Francisco"
+edad = 45
+print((f"{nombre:>10s}{edad:>5d}"))  # Alineación a la derecha
