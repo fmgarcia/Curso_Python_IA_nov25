6e13f939aa6c87e5ec6596474ce0c50054ec873e
fran <frangarcia2707@gmail.com>
2026-01-14 14:05:17 +0100

    api futbol mejorada

--- ene_14/leer_api_futbol_historial_liga.py
+++ ene_14/leer_api_futbol_historial_liga.py
@@ -1,12 +1,14 @@
+import time
 import requests
 import json
+import csv
 
 api_key = "6bff5b66e01940a4a4730dad785bbab3"
-url = f"http://api.football-data.org/v4/competitions/PD/standings?season=2023"
+ruta_fichero = './ficheros/clasificacion_liga.csv'
 
 
-def obtener_clasificacion():
-    headers = {"X-Auth-Token": api_key, "User-Agent": "PostmanRuntime/7.51.0"}
+def obtener_clasificacion(url):
+    headers = {"X-Auth-Token": api_key}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -19,7 +21,17 @@ def obtener_clasificacion():
                 team_name = position["team"]["name"]
                 points = position["points"]
                 played_games = position["playedGames"]
-                print(f"{position['position']}. {team_name} - Puntos: {points}, Partidos jugados: {played_games}")
+                won = position["won"]
+                drawn = position["draw"]
+                lost = position["lost"]
+                goals_for = position["goalsFor"]
+                goals_against = position["goalsAgainst"]
+                goal_difference = position["goalDifference"]
+                # Guardar datos en csv
+                with open(ruta_fichero, mode="a", newline="", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerow([url[-4:], position['position'], team_name, points, played_games, won, drawn, lost, goals_for, goals_against, goal_difference])
+            time.sleep(6)  # Pausa para evitar saturar la API
         else:
             print("No se encontraron datos de clasificación.")
     else:
@@ -27,4 +39,6 @@ def obtener_clasificacion():
 
 
 if __name__ == "__main__":
-    obtener_clasificacion()
+    for anyo in range(2015, 2026):   
+        url = f"http://api.football-data.org/v4/competitions/PD/standings?season={anyo}"
+        obtener_clasificacion(url)
